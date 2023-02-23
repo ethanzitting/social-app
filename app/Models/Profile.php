8920fa9fe3146dbd1786f78a6099ca4975e86a25
Dylan Schwindt <dschwindt@buildmidwestern.comm>
2023-02-23 15:13:05 -0600

    [WIP] Created the Profile Show and 404 Routes, Controller and Models

---
+++ app/Models/Profile.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Profile extends Model
+{
+    use HasFactory;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'slug',
+        'bio',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'user_id',
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * Get the user that owns the profile.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
